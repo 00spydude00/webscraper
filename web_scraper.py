aeff48f36dfcea7b4db240968bc2192c26aec188
00spydude00 <00spydude00@gmail.com>
2024-01-31 18:46:56 -0700

    reverted

--- web_scraper.py
+++ web_scraper.py
@@ -1,15 +1,16 @@
-# import modules
+# Import modules
 from bs4 import BeautifulSoup
 import requests
-# import page url
+# Import page url
 url = "https://realpython.github.io/fake-jobs/"
-# scrape and parse html
+# Scrape and parse html
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 jobs = soup.find_all("h2")
 job_elements = [
     h2_element.parent.parent.parent for h2_element in jobs
 ]
+# Print Scraped stuff
 for job_element in job_elements:
     title = job_element.find("h2", class_="title").text.strip()
     company = job_element.find("h3", class_="company").text.strip()
